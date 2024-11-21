6a835b8c9e5f423d34824e7bbded24d4a0415603
rsproj7 <rodolfoparanhos92@gmail.com>
2024-11-21 11:21:25 -0300

    up

--- script/git-fetch-all-proj.sh
+++ script/git-fetch-all-proj.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo Fetching working projects
+
 git clone git@github.com:ropsoft7/rsp-arch.git
 git clone git@github.com:ropsoft7/rsp-asset.git
 git clone git@github.com:ropsoft7/rsp-remake.git
 git clone git@github.com:ropsoft7/rsp-fsystem.git
+
+echo Fetching npm projects
+
+git clone git@github.com:ropsoft7/rsp-proman.git
+git clone git@github.com:ropsoft7/rsp-projman.git
+git clone git@github.com:ropsoft7/rsp-libjscript.git
 
 exit 0
